1f9ffadb714833f9c29d6ec6657dcc0da6381c00
codeninjaug <segayiandrew@gmail.com>
2022-10-16 15:20:17 +0300

    resolved datetime issues conversion

--- components/Header.js
+++ components/Header.js
@@ -39,8 +39,8 @@ function Header() {
        pathname:'/search',
        query:{
            location: searchInput,
-           startDate: startDate.toISOString(),
-           endDate: endDate.toISOString(),
+           startDate: startDate.toString(),
+           endDate: endDate.toString(),
            noOfGuests,
        },
     });
@@ -73,7 +73,7 @@ function Header() {
           <p className='hidden md:inline cursor-pointer'> Become a Host</p>
           <GlobeAltIcon className='h-6'/>
           <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
-              <MenuIcon className='h-6'/>
+              <MenuIcon  className='h-6'/>
               <UserCircleIcon className='h-6'/>
           </div>
        </div>
